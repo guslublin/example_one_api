8732cf512a2f73b38de2ce2fd557b23240d07171
Gustavo Lublin <glublin.desarrollo@jjemp.com>
2024-10-17 16:19:01 -0300

    Despliegue con Docker

--- example_one_api/settings_old.py
+++ example_one_api/settings_old.py
@@ -80,24 +80,13 @@ WSGI_APPLICATION = 'example_one_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'exampledb',  # Nombre de la base de datos según docker-compose.yml
-        'USER': 'exampleuser',  # Usuario de la base de datos según docker-compose.yml
-        'PASSWORD': 'examplepass',  # Contraseña según docker-compose.yml
-        'HOST': 'db',  # El nombre del servicio de la base de datos en docker-compose.yml
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
